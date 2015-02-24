4fd85e16047a93f56fe73a55145478c3892ef8ed
arkulo56 <arkulo@126.com>
2015-02-24 20:50:04 +0800

    2050

---
+++ fotosay/类图基本关系.mm
@@ -0,0 +1,49 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1424781080543" ID="ID_1594243866" MODIFIED="1424781116179" TEXT="&#x7c7b;&#x56fe;&#x57fa;&#x672c;&#x5173;&#x7cfb;">
+<node CREATED="1424782012033" ID="ID_350102270" MODIFIED="1424782052639" POSITION="right" TEXT="&#x7ee7;&#x627f;&#x7c7b;&#x578b;&#xff08;&#x4e09;&#x89d2;&#xff09;">
+<node CREATED="1424781148974" ID="ID_1792758791" MODIFIED="1424781930185" TEXT="&#x6cdb;&#x5316;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424781231839" ID="ID_217409459" MODIFIED="1424781236075" TEXT="&#x7ee7;&#x627f;&#x975e;&#x62bd;&#x8c61;&#x7c7b;"/>
+<node CREATED="1424781243331" ID="ID_1295355307" MODIFIED="1424781455238" TEXT="&#x7a7a;&#x5fc3;&#x4e09;&#x89d2;&#x5b9e;&#x7ebf;&#x7bad;&#x5934;"/>
+</node>
+<node CREATED="1424781268991" ID="ID_621532853" MODIFIED="1424781997124" TEXT="&#x5b9e;&#x73b0;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424781271573" ID="ID_23207552" MODIFIED="1424781275781" TEXT="&#x7ee7;&#x627f;&#x62bd;&#x8c61;&#x7c7b;"/>
+<node CREATED="1424781276522" ID="ID_489343408" MODIFIED="1424781466431" TEXT="&#x7a7a;&#x5fc3;&#x4e09;&#x89d2;&#x865a;&#x7ebf;&#x7bad;&#x5934;"/>
+</node>
+</node>
+<node CREATED="1424782037579" ID="ID_1941851094" MODIFIED="1424782101742" POSITION="right" TEXT="&#x6574;&#x4f53;&#x90e8;&#x5206;&#x7c7b;&#x578b;&#xff08;&#x83f1;&#x5f62;&#xff09;">
+<node CREATED="1424781348390" ID="ID_1061177047" MODIFIED="1424781933040" TEXT="&#x805a;&#x5408;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424781370795" ID="ID_38083567" MODIFIED="1424781376373" TEXT="&#x6574;&#x4f53;&#x6709;&#x90e8;&#x5206;&#x6784;&#x6210;">
+<node CREATED="1424781401028" ID="ID_258336258" MODIFIED="1424781410169" TEXT="&#x4e0d;&#x662f;&#x5f3a;&#x4f9d;&#x8d56;&#x5173;&#x7cfb;"/>
+</node>
+<node CREATED="1424781379421" ID="ID_926105663" MODIFIED="1424781448606" TEXT="&#x7a7a;&#x5fc3;&#x83f1;&#x5f62;&#x5b9e;&#x7ebf;&#x7bad;&#x5934;"/>
+</node>
+<node CREATED="1424781473573" ID="ID_353299473" MODIFIED="1424781934276" TEXT="&#x7ec4;&#x5408;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424781505242" ID="ID_213103674" MODIFIED="1424781513491" TEXT="&#x6574;&#x4f53;&#x7531;&#x90e8;&#x5206;&#x6784;&#x6210;">
+<node CREATED="1424781513928" ID="ID_1728124606" MODIFIED="1424781533220" TEXT="&#x5f3a;&#x4f9d;&#x8d56;&#x5173;&#x7cfb;&#xff0c;&#x6574;&#x4f53;&#x6d88;&#x4ea1;&#xff0c;&#x90e8;&#x5206;&#x4e5f;&#x6ca1;&#x6709;&#x5b58;&#x5728;&#x7684;&#x4ef7;&#x503c;"/>
+</node>
+<node CREATED="1424781549340" ID="ID_1419291222" MODIFIED="1424781557600" TEXT="&#x5b9e;&#x5fc3;&#x83f1;&#x5f62;&#x5b9e;&#x7ebf;&#x7bad;&#x5934;"/>
+</node>
+</node>
+<node CREATED="1424782104155" ID="ID_796066774" MODIFIED="1424782106432" POSITION="right" TEXT="&#x5173;&#x7cfb;&#x7c7b;&#x578b;">
+<node CREATED="1424781582398" ID="ID_1912610954" MODIFIED="1424781935448" TEXT="&#x5173;&#x8054;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424781611805" ID="ID_1417290928" MODIFIED="1424781852466" TEXT="&#x9759;&#x6001;&#x7684;&#x4e00;&#x79cd;&#x5173;&#x8054;&#x5173;&#x7cfb;&#xff08;&#x5f3a;&#x5173;&#x8054;&#xff09;">
+<node CREATED="1424781666173" ID="ID_1000112318" MODIFIED="1424781694471" TEXT="&#x6210;&#x5458;&#x53d8;&#x91cf;&#x7684;&#x5f62;&#x5f0f;&#x5b58;&#x5728;&#xff0c;&#x4f8b;&#x5982;&#x4eba;&#x548c;&#x8eab;&#x4efd;&#x8bc1;"/>
+</node>
+<node CREATED="1424781652715" ID="ID_1475279539" MODIFIED="1424781903600" TEXT="&#x5b9e;&#x7ebf;"/>
+</node>
+<node CREATED="1424781716789" ID="ID_24749961" MODIFIED="1424781936456" TEXT="&#x4f9d;&#x8d56;">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1424781786952" ID="ID_1348686435" MODIFIED="1424781792931" TEXT="&#x52a8;&#x6001;&#x7684;&#x4e00;&#x79cd;&#x5173;&#x8054;&#x5173;&#x7cfb;">
+<node CREATED="1424781793347" ID="ID_686469915" MODIFIED="1424781881301" TEXT="&#x4eba;&#x548c;&#x81ea;&#x884c;&#x8f66;&#x672c;&#x6765;&#x6ca1;&#x6709;&#x5f3a;&#x5173;&#x8054;&#x5173;&#x7cfb;&#xff0c;&#x4f46;&#x662f;&#x8981;&#x8d76;&#x8def;&#xff0c;&#x4eba;&#x548c;&#x81ea;&#x884c;&#x8f66;&#x5c31;&#x4ea7;&#x751f;&#x4e86;&#x4f9d;&#x8d56;&#xff01;&#x4eba;&#x548c;&#x8eab;&#x4efd;&#x8bc1;&#x5c31;&#x4e0d;&#x4e00;&#x6837;&#x4e86;"/>
+</node>
+<node CREATED="1424781911490" ID="ID_1529852543" MODIFIED="1424781917704" TEXT="&#x4e09;&#x53c9;&#x865a;&#x7ebf;&#x7bad;&#x5934;"/>
+</node>
+</node>
+</node>
+</map>
