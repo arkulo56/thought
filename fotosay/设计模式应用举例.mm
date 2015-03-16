967fb6b2872167dfec271e360a1a2749184229c5
arkulo56 <arkulo@126.com>
2015-03-16 22:10:27 +0800

    2210

---
+++ fotosay/设计模式应用举例.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1426512365664" ID="ID_1487900426" MODIFIED="1426512401605" TEXT="&#x8bbe;&#x8ba1;&#x6a21;&#x5f0f;&#x5e94;&#x7528;&#x4e3e;&#x4f8b;">
+<node CREATED="1426512375350" ID="ID_1514320351" MODIFIED="1426512377092" POSITION="right" TEXT="&#x547d;&#x4ee4;&#x6a21;&#x5f0f;">
+<node CREATED="1426512377851" ID="ID_1983203667" MODIFIED="1426512952814" TEXT="&#x5b8f;&#x547d;&#x4ee4;&#xff1a;&#x628a;&#x4e00;&#x4e2a;&#x4e2a;&#x7684;&#x547d;&#x4ee4;&#x8bb0;&#x4e0b;&#x6765;&#xff0c;&#x7136;&#x540e;&#x8fd8;&#x80fd;&#x6267;&#x884c;&#x4e00;&#x904d;"/>
+<node CREATED="1426512909549" ID="ID_240268340" MODIFIED="1426512925387" TEXT="&#x9065;&#x63a7;&#x5668;"/>
+</node>
+</node>
+</map>
